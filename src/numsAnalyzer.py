4a8e8c4ae41c60245fa1cad55eb6f9e1534d17ca
Kevin Tao <kevin.tao@stonybrook.edu>
2022-10-09 17:08:35 -0400

    numsAnalyzer + tests done

--- src/numsAnalyzer.py
+++ src/numsAnalyzer.py
@@ -1,14 +1,31 @@
 # Run this script from the repository's root.
+from re import A
 import numpy as np
 
 def replaceMissingValues(x):
-    pass
+    return np.nan_to_num(x)
 
 def countMissingValues(x, k=0):
-    pass
+    try:
+        return np.sum(np.isnan(x),axis=k)
+    except (np.AxisError, TypeError): raise ValueError
 
 def exams_with_median_gt_K(x, k):
-    pass
+    if k < 0 or k > 100: raise ValueError
+    if not isinstance(k, int): raise TypeError
+    y = replaceMissingValues(x)
+    if np.max(y) > 100 or np.min(y) < 0: raise ValueError
+    return np.sum(np.median(y, axis=1) > k) #Sum of the number of rows that have median > k
 
 def curve_low_scoring_exams(x, k):
-    pass
+    if k < 0 or k > 100: raise ValueError
+    y = replaceMissingValues(x)
+    if np.max(y) > 100 or np.min(y) < 0: raise ValueError
+    if not isinstance(k, int): raise TypeError
+    #(1) Replace NaN with 0s. (2) Take the max of each row. (3) Reshape it to (4,1). 
+    #(4) Subtract it from 100 to get the weight points. (5) Multiply by the filter to zero out means >= k
+    weight = np.reshape(np.mean(y,axis=1) < k,(4,1)) * np.reshape(100-np.max(y, axis=1), (4,1))
+    #(6) Add it to the original array to get weighted scores.
+    weightedScores = y+weight
+    #(7) Sort by indices from calling argsort on the mean array. (8) Round to 1st decimal place.
+    return np.round(weightedScores[np.argsort(np.mean(y,axis=1))],decimals=1)
--- tests/numsAnalyzer_test.py
+++ tests/numsAnalyzer_test.py
@@ -4,4 +4,135 @@ sys.path.insert(1, os.getcwd())
 from src.numsAnalyzer import np, countMissingValues, curve_low_scoring_exams, exams_with_median_gt_K
 
 class TestNumsAnalyzer:
-    pass
+    def test_countMissingValues_PosAxis(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        assert np.array_equal(countMissingValues(input,0), np.array([0, 2, 0, 3, 0]))
+        assert np.array_equal(countMissingValues(input,1), np.array([0, 1, 2, 2]))
+    def test_countMissingValues_NegAxis(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        assert np.array_equal(countMissingValues(input,-2), np.array([0, 2, 0, 3, 0]))
+        assert np.array_equal(countMissingValues(input,-1), np.array([0, 1, 2, 2]))
+    def test_countMissingValues_ThreeDimensional(self):
+        input = np.array([[[100.0, 87.3, 94.5, 99.0, 78.4],[82.6, 71.3, 99.9, np.NaN, 48.0]],
+                          [[92.6, np.NaN, 43.5, np.NaN, 80.0],[97.0, np.NaN, 98.5, np.NaN, 65.3]]])
+        assert np.array_equal(countMissingValues(input, 0), np.array([[0, 1, 0, 1, 0],[0, 1, 0, 2, 0]]))
+        assert np.array_equal(countMissingValues(input, 1), np.array([[0, 0, 0, 1, 0],[0, 2, 0, 2, 0]]))
+        assert np.array_equal(countMissingValues(input, 2), np.array([[0, 1],[2, 2]]))
+    def test_countMissingValues_InvalidAxis(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            countMissingValues(input, 3)
+    def test_countMissingValues_NonIntAxis(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            countMissingValues(input, 1.5)
+    def test_medianK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        # Medians = [94.5, 71.3, 43.5, 65.3]
+        assert exams_with_median_gt_K(input, 95) == 0
+        assert exams_with_median_gt_K(input, 90) == 1
+        assert exams_with_median_gt_K(input, 70) == 2
+        assert exams_with_median_gt_K(input, 60) == 3
+        assert exams_with_median_gt_K(input, 40) == 4
+    def test_medianK_NegativeK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            exams_with_median_gt_K(input, -1)
+    def test_medianK_BigK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            exams_with_median_gt_K(input, 101)
+    def test_medianK_ArrayHasNegatives(self):
+        input = np.array([[100.0, 87.3, 94.5, -99.0, 78.4],
+                          [82.6, -71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, -98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            exams_with_median_gt_K(input, 70)
+    def test_medianK_ArrayHasBigValues(self):
+        input = np.array([[100.0, 87.3, 94.5, 199.0, 78.4],
+                          [82.6, 71.3, 199.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [197.0, np.NaN, 98.5, np.NaN, 165.3]])
+        with pytest.raises(ValueError):
+            exams_with_median_gt_K(input, 70)
+    def test_medianK_NonIntK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(TypeError):
+            exams_with_median_gt_K(input, 80.5)
+    def test_curveExams(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        expected = np.array([[100.0, 7.4, 50.9, 7.4, 87.4],
+                             [98.5, 1.5, 100.0, 1.5, 66.8],
+                             [82.7, 71.4, 100.0, 0.1, 48.1],
+                             [100.0, 87.3, 94.5, 99.0, 78.4]])
+        expected2 = np.array([[100.0, 7.4, 50.9, 7.4, 87.4],
+                             [98.5, 1.5, 100.0, 1.5, 66.8],
+                             [82.6, 71.3, 99.9, 0, 48.0],
+                             [100.0, 87.3, 94.5, 99.0, 78.4]])
+        assert np.array_equal(curve_low_scoring_exams(input, 95), expected)
+        assert np.array_equal(curve_low_scoring_exams(input, 90), expected)
+        assert np.array_equal(curve_low_scoring_exams(input, 60), expected2)
+    def test_curveExams_NegativeK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            curve_low_scoring_exams(input, -1)
+    def test_curveExams_BigK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            curve_low_scoring_exams(input, 101)
+    def test_curveExams_ArrayHasNegatives(self):
+        input = np.array([[100.0, 87.3, 94.5, -99.0, 78.4],
+                          [82.6, -71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, -98.5, np.NaN, 65.3]])
+        with pytest.raises(ValueError):
+            curve_low_scoring_exams(input, 70)
+    def test_curveExams_ArrayHasBigValues(self):
+        input = np.array([[100.0, 87.3, 94.5, 199.0, 78.4],
+                          [82.6, 71.3, 199.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [197.0, np.NaN, 98.5, np.NaN, 165.3]])
+        with pytest.raises(ValueError):
+            curve_low_scoring_exams(input, 70)
+    def test_curveExams_NonIntK(self):
+        input = np.array([[100.0, 87.3, 94.5, 99.0, 78.4],
+                          [82.6, 71.3, 99.9, np.NaN, 48.0],
+                          [92.6, np.NaN, 43.5, np.NaN, 80.0],
+                          [97.0, np.NaN, 98.5, np.NaN, 65.3]])
+        with pytest.raises(TypeError):
+            curve_low_scoring_exams(input, 80.5)
+    
