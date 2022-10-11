36758c15f7bacae7e2474f38099c2994535c471b
Kevin Tao <kevin.tao@stonybrook.edu>
2022-10-10 22:31:46 -0400

    Rename files + tests done

--- src/rename_files.py
+++ src/rename_files.py
@@ -1,6 +1,16 @@
 # Run this script from the repository's root.
 import re
 import os
-
+# We learned shutil.move in class, but we can't use it here since it would require an import. idk if thats intentional?
 def rename(path):
-    pass
+    pat = re.compile("^snap[0-9]{3}.txt")
+    try:
+        files = [file for file in os.listdir(path) if pat.search(file) != None]
+    except: return
+    if files == []: return
+    files.sort()
+    fileNum = int(files[0][4:7])-1
+    for file in files:
+        if int(file[4:7]) != fileNum+1:
+            os.rename(path + file, path + "snap" + f"{(fileNum+1):03}" + ".txt")
+        fileNum += 1
--- tests/rename_file_test.py
+++ tests/rename_file_test.py
@@ -5,5 +5,36 @@ sys.path.insert(1, os.getcwd())
 from src.rename_files import rename
 import shutil
 
-class RenameFilesTest:
-    pass
+class TestRenameFiles:
+    def test_RenameFiles(self):
+        testPath = "./snaptest/"
+        if os.path.exists(testPath): shutil.rmtree(testPath)
+        os.mkdir(testPath)
+        os.mkdir(testPath + "dir/")
+        with open(testPath + "snap000.txt", 'w') as f: pass
+        with open(testPath + "snap001.txt", 'w') as f: pass
+        with open(testPath + "snap002.txt", 'w') as f: pass
+        with open(testPath + "snap004.txt", 'w') as f: pass
+        with open(testPath + "snap005.txt", 'w') as f: pass
+        with open(testPath + "snap008.txt", 'w') as f: pass
+        with open(testPath + "snap01.txt", 'w') as f: pass
+        with open(testPath + "nap002.txt", 'w') as f: pass
+        with open(testPath + "asdf.txt", 'w') as f: pass
+        with open(testPath + "dir/snap009.txt", 'w') as f: pass
+        rename(testPath); outfiles = os.listdir(testPath); outfiles.sort()
+        assert outfiles == ["asdf.txt","dir","nap002.txt","snap000.txt","snap001.txt","snap002.txt","snap003.txt","snap004.txt","snap005.txt","snap01.txt"]
+        outfiles = os.listdir(testPath + "dir/"); 
+        assert outfiles == ["snap009.txt"]
+    def test_RenameFiles_EmptyDir(self):
+        testPath = "./snaptest/"
+        if os.path.exists(testPath): shutil.rmtree(testPath)
+        os.mkdir(testPath)
+        rename(testPath); outfiles = os.listdir(testPath);
+        assert outfiles == []
+    def test_RenameFiles_InvalidDir(self):
+        testPath = "./snaptest/"
+        if os.path.exists(testPath): shutil.rmtree(testPath)
+        os.mkdir(testPath)
+        assert rename("garbage") == None
+        rename(testPath); outfiles = os.listdir(testPath);
+        assert outfiles == []
